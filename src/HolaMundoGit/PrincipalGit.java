c4723f9b00045b973d6e25222a1bd7c7c130ae0c
Felix <felixnicolas-m@hotmail.com>
2023-08-02 14:33:55 -0300

    Comienzo de proyecto - Declaración de variable nombre

---
+++ src/HolaMundoGit/PrincipalGit.java
@@ -0,0 +1,16 @@
+package HolaMundoGit;
+
+public class PrincipalGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba para git");
+		
+		String nombre= "Felix";
+		
+		System.out.println(nombre);
+		
+
+	}
+
+}
